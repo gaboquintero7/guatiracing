2f687759637696c3058f36b1ab8bc76935a90c1c
Gabriel Quintero <gabrielalejandroqd@gmail.com>
2017-05-01 23:29:22 -0300

    Commit inicial
    Commit inicial del prototipo del juego

---
+++ Assets/scripts/roteLlantas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roteLlantas : MonoBehaviour {
+
+	// grados que se va a rotar 
+	public int angleX;
+	public int angleY;
+
+	public void Update () {	
+		if (Input.GetKeyDown(KeyCode.W)){
+			transform.Rotate (Vector3.right, angleX);
+		}
+
+		if (Input.GetKeyDown(KeyCode.S)){
+			transform.Rotate (Vector3.left, angleX * (-1));
+		}
+
+		if (Input.GetKeyDown(KeyCode.A)){
+			transform.Rotate (Vector3.up, angleX);
+		}
+
+		if (Input.GetKeyDown(KeyCode.D)){
+			transform.Rotate (Vector3.up, angleX * (-1));
+		}
+
+	}
+}
